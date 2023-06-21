f19755fdefec7ddf0f1604353928af84c3594db3
omar.barrios <omar.barrios@ct.uneatlantico.es>
2023-06-21 12:23:36 +0200

    i removed files unnecessary

--- docs/casos/codigo/dd/v1/Hombre.java
+++
@@ -1,9 +0,0 @@
-package docs.casos.codigo.dd.v1;
-public class Hombre extends Persona {
-
-	public void recibirPalmada(Recepcionista recepcionista) {
-		System.out.println("---------- Soy un hombre lleno de vanidad");
-		recepcionista.recibirPropina(3);
-	}	
-	
-}
--- docs/casos/codigo/dd/v3/VisitadorPersona.java
+++
@@ -1,8 +0,0 @@
-package docs.casos.codigo.dd.v3;
-public interface VisitadorPersona {
-
-	void visitar(Mujer mujer);
-
-	void visitar(Hombre hombre);
-
-}
